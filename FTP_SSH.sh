a03cb508722f3589c6c3982bc3edbfd3563bd12c
ShadowT93 <43940992+ShadowT93@users.noreply.github.com>
2018-10-08 05:29:59 +0400

    Update FTP_SSH.sh

--- FTP_SSH.sh
+++ FTP_SSH.sh
@@ -6,15 +6,18 @@ mkdir /etc/ssl/certificates;
 openssl req -x509 -nodes -days 356 -newkey rsa:2048 -keyout /etc/ssl/certificates/vsftpd.pem -out /etc/ssl/certificates/vsftpd.pem;
 sed -i  "s/anonymous_enable=YES/anonymous_enable=NO/gI" /etc/vsftpd.conf;
 sed -i  "s/write_enable=YES/write_enable=NO/gI" /etc/vsftpd.conf;
-sed -i "s/'rsa_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem'/'rsa_cert_file=/etc/ssl/certificates/vsftpd.pem'/gI" /etc/vsftpd.conf;
-sed -i "s/'rsa_private_key_file=/etc/ssl/private/ssl-cert-snakeoil.key'/'rsa_private_key_file=/etc/ssl/certificates/vsftpd.pem'/gI" /etc/vsftpd.conf;
+sed -i "s|rsa_cert_file=.*|rsa_cert_file=/etc/ssl/certificates/vsftpd.pem|gI" /etc/vsftpd.conf;
+sed -i "s|rsa_private_key_file=.*|rsa_private_key_file=/etc/ssl/certificates/vsftpd.pem|gI" /etc/vsftpd.conf;
 sed -i  "s/ssl_tlsv2=NO/ssl_sslv2=NO/gI" /etc/vsftpd.conf;
 sed -i  "s/ssl_tlsv3=NO/ssl_sslv3=NO/gI" /etc/vsftpd.conf;
 sed -i  "s/write_enable=YES/write_enable=NO/gI" /etc/vsftpd.conf;
 sed -i  "s/anon_mkdir_write_enable=YES/anon_mkdir_write_enable=NO/gI" /etc/vsftpd.conf;
 sed -i  "s/anon_upload_enable=YES/anon_upload_enable=NO/gI" /etc/vsftpd.conf;
 
-echo "ssl_sslv2=NO
+echo "force_local_data_ssl=YES
+force_local_logins_ssl=YES
+ssl_tlsv1=YES
+ssl_sslv2=NO
 ssl_sslv3=NO" >> /etc/vsftpd.conf;
 
 service vsftpd restart;
